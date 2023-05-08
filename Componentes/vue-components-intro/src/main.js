1fe25b3fbddf1043798db9c93ac8bb7b0e8ef71c
Luana Freitas <luanacassiaf@gmail.com>
2023-05-07 21:27:00 -0300

    Criação e importação de componentes

--- Componentes/vue-components-intro/src/main.js
+++ Componentes/vue-components-intro/src/main.js
@@ -1,56 +1,7 @@
 import Vue from 'vue'
-
+import App from './App.vue'
 Vue.config.productionTip = false
 
-// Vue.component('my-component', {
-//   data: function() {
-//     return {
-//       pokemon: 'Pikachu',
-//       level: 0
-//     }
-//   },
-//   methods: {
-//     levelUp: function() {
-//       this.level++
-//     }
-//   },
-//   template: `
-//       <div>
-//         <h1>{{pokemon}}</h1>
-//         <h2>Level {{level}}</h2>
-//         <button @click="levelUp">Up</button>
-//       </div>
-//     `
-// })
-
-const myComponent = {
-  data: function() {
-    return {
-      pokemon: 'Pikachu',
-      level: 0
-    }
-  },
-  methods: {
-    levelUp: function() {
-      this.level++
-    }
-  },
-  template: `
-      <div>
-        <h1>{{pokemon}}</h1>
-        <h2>Level {{level}}</h2>
-        <button @click="levelUp">Up</button>
-      </div>
-    `
-}
-
 new Vue({
-  el: '#app1',
-  components: {
-    'my-component': myComponent // Registro local de componente
-  }
-})
-
-new Vue({
-  el: '#app2'
-})
+  render: h => h(App)
+}).$mount('#app')
