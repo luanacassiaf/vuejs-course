28495443afc5cdb621937f205c4b64aa738dfa64
Luana Freitas <luanacassiaf@gmail.com>
2023-05-07 21:04:12 -0300

    Criação de componente

---
+++ Componentes/vue-components-intro/src/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+
+Vue.config.productionTip = false
+
+Vue.component('my-component', {
+  data: function() {
+    return {
+      pokemon: 'Pikachu',
+      level: 0
+    }
+  },
+  methods: {
+    levelUp: function() {
+      this.level++
+    }
+  },
+  template: `
+      <div>
+        <h1>{{pokemon}}</h1>
+        <h2>Level {{level}}</h2>
+        <button @click="levelUp">Up</button>
+      </div>
+    `
+})
+
+new Vue({
+  el: '#app'
+})
