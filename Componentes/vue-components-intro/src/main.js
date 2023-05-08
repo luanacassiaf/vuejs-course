74491fee7127d74df6641364aea13276348bc6ec
Luana Freitas <luanacassiaf@gmail.com>
2023-05-07 21:10:13 -0300

    Registro local de componente

--- Componentes/vue-components-intro/src/main.js
+++ Componentes/vue-components-intro/src/main.js
@@ -2,7 +2,28 @@ import Vue from 'vue'
 
 Vue.config.productionTip = false
 
-Vue.component('my-component', {
+// Vue.component('my-component', {
+//   data: function() {
+//     return {
+//       pokemon: 'Pikachu',
+//       level: 0
+//     }
+//   },
+//   methods: {
+//     levelUp: function() {
+//       this.level++
+//     }
+//   },
+//   template: `
+//       <div>
+//         <h1>{{pokemon}}</h1>
+//         <h2>Level {{level}}</h2>
+//         <button @click="levelUp">Up</button>
+//       </div>
+//     `
+// })
+
+const myComponent = {
   data: function() {
     return {
       pokemon: 'Pikachu',
@@ -21,8 +42,15 @@ Vue.component('my-component', {
         <button @click="levelUp">Up</button>
       </div>
     `
+}
+
+new Vue({
+  el: '#app1',
+  components: {
+    'my-component': myComponent // Registro local de componente
+  }
 })
 
 new Vue({
-  el: '#app'
+  el: '#app2'
 })
