d2ac7ab90ea678d7c45805c2655222f73a7e2b0b
Luana Freitas <luanacassiaf@gmail.com>
2023-07-02 21:10:42 -0300

    Slots

---
+++ Componentes/vue-components-avancado/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app" class="container">
+    <h1>Vue JS</h1>
+
+    <Post titulo="Passagem de dados via props">
+      <h2 slot="cabecalho">Lorem ipsum</h2>
+      <template slot="conteudo">
+        <p>Mussum Ipsum, cacilds vidis litro abertis.</p>
+        <p>Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu levo!</p>
+      </template>
+      <small slot="rodape">by Mussum</small>
+    </Post>
+
+    </div>
+</template>
+
+<script>
+
+import Post from './components/Post.vue'
+
+export default {
+  components: {
+    Post
+  },
+}
+</script>
+
+<style scoped>
+  .container {
+    width: 960px;
+    margin: auto;
+  }
+</style>
---
+++ Componentes/vue-components-avancado/src/components/Post.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="post">
+    <div class="cabecalho">
+      <slot name="cabecalho"></slot>
+    </div>
+    <div class="conteudo">
+      <slot name="conteudo"></slot>
+    </div>
+    <div class="rodape">
+      <slot name="rodape"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .post {
+    margin: 15px auto;
+  }
+
+  .cabecalho {
+    padding: 5px 12px;
+    background-color: #eee;
+  }
+
+  .conteudo {
+    font-size: 16px;
+  }
+
+  .rodape {
+    font-style: italic;
+  }
+</style>
