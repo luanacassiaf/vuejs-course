4413dce4cb865903251ffc3f38761550a80acf9e
Luana Freitas <luanacassiaf@gmail.com>
2023-10-07 17:10:42 -0300

    Estrutura inicial de rotas

---
+++ Vue Router/vue-routes/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="app">
+    <div class="jumbotron">
+      <h1 class="display-4">Vue Router</h1>
+      <p class="lead">Adicionando rotas a Single Page Applications no Vue.</p>
+    </div>
+
+    <div class="container">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
