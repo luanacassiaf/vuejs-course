8d73020b616adbc64274dd22a52080274161cf34
Luana Freitas <luanacassiaf@gmail.com>
2023-10-21 18:17:15 -0300

    Query params

--- Vue Router/vue-routes/src/components/contatos/ContatosLista.vue
+++ Vue Router/vue-routes/src/components/contatos/ContatosLista.vue
@@ -1,47 +1,72 @@
 <template>
-	<div>
-		<h3 class="font-weight-light">Contatos</h3>
+  <div>
+    <h3 class="font-weight-light">Contatos</h3>
 
-		<ul class="list-group" v-if="contatos.length > 0">
-			<contatos-lista-item
-				class="list-group-item"
-				v-for="contato in contatos"
-				:key="contato.id"
-				:contato="contato"
-			/>
-		</ul>
+    <div class="form-group">
+      <input
+        type="search"
+        class="form-control"
+        placeholder="Buscar contatos"
+        @keyup.enter="buscar"
+        :value="$route.query.busca"
+      />
+    </div>
 
-		<p v-else>Nenhum contato cadastrado.</p>
+    <ul class="list-group" v-if="contatosFiltrados.length > 0">
+      <contatos-lista-item
+        class="list-group-item"
+        v-for="contato in contatosFiltrados"
+        :key="contato.id"
+        :contato="contato"
+      />
+    </ul>
 
-		<button class="btn btn-secondary mt-4 mb-4" @click="voltar">Voltar</button>
-	</div>
+    <p v-else>Nenhum contato cadastrado.</p>
+
+    <button class="btn btn-secondary mt-4 mb-4" @click="voltar">Voltar</button>
+  </div>
 </template>
 
 <script>
-
-import ContatosListaItem from './ContatosListaItem.vue'
+import ContatosListaItem from "./ContatosListaItem.vue";
 
 export default {
-	components: {
-		ContatosListaItem
-	},
-	data() {
-		return {
-			contatos: [
-				{id: 1, nome: 'Goku', email: 'goku@capsule.com'},
-				{id: 2, nome: 'Gohan', email: 'gohan@capsule.com'},
-				{id: 3, nome: 'Goten', email: 'goten@capsule.com'}
-			]
-		}
-	},
-	methods: {
-		voltar() {
-			// this.$router.push('/') adiciona nova entrada ao histórico de navegação
-			// this.$router.replace('/') substitui entrada no histórico
-			// this.$router.go(1) // retornar ou avançar x registros no histórico
-			// this.$router.foward() avança 1 registro no histórico
-			this.$router.back() // retorna 1 registro no histórico
-		}
-	},
-}
+  components: {
+    ContatosListaItem,
+  },
+  data() {
+    return {
+      contatos: [
+        { id: 1, nome: "Goku", email: "goku@capsule.com" },
+        { id: 2, nome: "Gohan", email: "gohan@capsule.com" },
+        { id: 3, nome: "Goten", email: "goten@capsule.com" },
+      ],
+    };
+  },
+  computed: {
+    contatosFiltrados() {
+      const busca = this.$route.query.busca;
+      return !busca
+        ? this.contatos
+        : this.contatos.filter((contato) =>
+            contato.nome.toLowerCase().includes(busca.toLowerCase())
+          );
+    },
+  },
+  methods: {
+    buscar(event) {
+      this.$router.push({
+        path: "/contatos",
+        query: { busca: event.target.value },
+      });
+    },
+    voltar() {
+      // this.$router.push('/') adiciona nova entrada ao histórico de navegação
+      // this.$router.replace('/') substitui entrada no histórico
+      // this.$router.go(1) // retornar ou avançar x registros no histórico
+      // this.$router.foward() avança 1 registro no histórico
+      this.$router.back(); // retorna 1 registro no histórico
+    },
+  },
+};
 </script>
