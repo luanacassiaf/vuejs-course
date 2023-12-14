b8167fb698d596d600febda201fc8868dba7a705
Luana Freitas <luanacassiaf@gmail.com>
2023-12-13 21:50:56 -0300

    Lista de tarefas com axios

---
+++ Comunicação HTTP/vue-http/src/components/TarefasLista.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-sm-10">
+                <h1 class="font-weight-light">Lista de tarefas</h1>
+            </div>
+            <div class="col-sm-2">
+                <button class="btn btn-primary float-right"
+                    @click="exibirForm = !exibirForm">
+                    <i class="fa fa-plus mr-2"></i>
+                    <span>Criar</span>
+                </button>
+            </div>
+        </div>
+
+        <ul class="list-group" v-if="tarefas.length > 0">
+            <TarefasListaIten
+                v-for="tarefa in tarefas"
+                :key="tarefa.id"
+                :tarefa="tarefa" />
+        </ul>
+
+        <p v-else>Nenhuma tarefa criada.</p>
+
+        <TarefaSalvar v-if="exibirForm" @criar="criarTarefa"/>
+
+    </div>
+</template>
+
+<script>
+
+import axios from 'axios'
+import config from './../config/config'
+import TarefaSalvar from './TarefaSalvar.vue'
+import TarefasListaIten from './TarefasListaIten.vue'
+
+export default {
+    components: {
+        TarefaSalvar,
+        TarefasListaIten
+    },
+    data() {
+        return {
+            tarefas: [],
+            exibirForm: false
+        }
+    },
+    created() {
+        axios.get(`${config.apiURL}/tarefas`)
+            .then((response) => {
+                this.tarefas = response.data
+            })
+    },
+    methods: {
+        criarTarefa(tarefa) {
+            axios.post(`${config.apiURL}/tarefas`, tarefa)
+            .then(response => {
+                this.tarefas.push(response.data)
+                this.exibirForm = false
+            })
+        }
+    }
+}
+</script>
